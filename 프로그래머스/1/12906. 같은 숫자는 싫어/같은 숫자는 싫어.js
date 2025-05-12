9fd50a1a5e9138da57292fc35afa9f2bbe23707f
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-12 15:34:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.74 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr)
+{
+    return arr.reduce((acc, cur) => {
+        if (acc[acc.length - 1] !== cur) {
+            acc.push(cur);
+        }
+        return acc;
+    }, [])
+}
