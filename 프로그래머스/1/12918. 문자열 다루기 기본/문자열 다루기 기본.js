6a900df0acbe9899722c5af2aa29dc623bd83653
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-12 14:44:58 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,3 +1,4 @@
 function solution(s) {
-    return /^[0-9]{4}$|^[0-9]{6}$/.test(s);
+    // return /^[0-9]{4}$|^[0-9]{6}$/.test(s);
+    return (s.length === 4 || s.length === 6) && !s.split("").map(n => isNaN(Number(n))).includes(true);
 }
