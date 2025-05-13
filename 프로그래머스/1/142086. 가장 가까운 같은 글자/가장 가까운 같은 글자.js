8ec5df98337dcd0221fd8bbb00a489610d758ffd
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-13 17:12:21 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 98.67 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,9 +1,7 @@
 function solution(s) {
     let loc = 0;
-    let result = [];
-    s.split("").map((i,idx) => {
+    return s.split("").map((i,idx) => {
         loc = s.slice(0, idx).split("").lastIndexOf(i);
-        result.push(loc < 0 ? loc : idx - loc)
+        return loc < 0 ? loc : idx - loc;
     })
-    return result;
 }
