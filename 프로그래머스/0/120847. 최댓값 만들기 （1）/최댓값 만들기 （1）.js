56942b7122feaa5265a33c80de35d3c389172e4f
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-22 20:16:36 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,9 +1,4 @@
 function solution(numbers) {
-    let arr = [];
-    for (let i = 0; i < numbers.length - 1; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            arr.push(numbers[i] * numbers[j]);
-        }        
-    }
-    return Math.max(...arr);
+    numbers.sort((a, b) => b - a);
+    return numbers[0] * numbers[1];
 }
