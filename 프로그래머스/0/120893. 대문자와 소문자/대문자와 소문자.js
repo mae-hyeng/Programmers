17b7e2a7913eaeeb1180ef2fdaef55a5b1e32a68
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-23 19:06:04 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-    return [...my_string].map(ms => {
-        let charCode = ms.charCodeAt();
-        if (65 <= charCode && charCode <= 90) charCode += 32
-        else charCode -= 32;
-        return String.fromCharCode(charCode);
-    }).join("")
+    return [...my_string].map(ms => ms === ms.toLowerCase() ? ms.toUpperCase() : ms.toLowerCase()).join("")
 }
