eb1f7eacfafe4988af7de0019f3924eff5319502
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-24 13:12:00 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 2.89 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    let count = 0;
+    let loop = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i][j] === arr[j][i]) count++;
+            loop++;
+        }
+    }
+    return count === loop ? 1 : 0;
+}
