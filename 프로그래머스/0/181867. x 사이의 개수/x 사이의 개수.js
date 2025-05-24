4e46883d5405eeec7289fac5d85bb31b229e36a9
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-24 14:16:02 +0900

    [level 0] Title: x 사이의 개수, Time: 2.62 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,12 +1,3 @@
 function solution(myString) {
-    let arr = [];
-    let count = 0;
-    [...myString].forEach(ms => {
-        if (ms !== "x") count++;
-        else {
-            arr.push(count);
-            count = 0;
-        }
-    });
-    return [...arr, count];
+    return myString.split("x").map(ms => ms.length);
 }
