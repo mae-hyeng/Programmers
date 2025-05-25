804dcceae259b68f89694d49568bff26652df101
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-25 21:16:51 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,13 +1,7 @@
 function solution(str_list) {
-    const li = str_list.indexOf("l");
-    const ri = str_list.indexOf("r");
-    let flag = true;
-    if (li === -1 && ri === -1) return [];
-    if (li === -1) return str_list.slice(ri + 1);
-    if (ri === -1) return str_list.slice(0, li);
-    if (li > -1 && ri > -1) {
-        flag = li < ri ? true : false;
+    for(let i = 0; i < str_list.length; i++) {
+        if (str_list[i] === 'l') return str_list.slice(0, i);
+        if (str_list[i] === 'r') return str_list.slice(i + 1);
     }
-    
-    return flag ? str_list.slice(0, li) : str_list.slice(ri + 1);
+    return [];
 }
