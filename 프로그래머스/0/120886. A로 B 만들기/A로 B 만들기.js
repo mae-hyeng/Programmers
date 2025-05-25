6c1f054f7e323e67a9e56e124f166a0c711fbb06
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-05-25 15:38:32 +0900

    [level 0] Title: A로 B 만들기, Time: 0.76 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,6 +1,6 @@
 function solution(before, after) {
     function sorted (value) {
-        return value.split("").sort((a, b) => a.localeCompare(b)).join("");
+        return value.split("").sort().join("");
     }
     return sorted(before) === sorted(after) ? 1 : 0;
 }
