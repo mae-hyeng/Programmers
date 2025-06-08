050545f02ebdc5d391bfc1b3b4edb1e70cce91d7
mae-hyeng <101870338+mae-hyeng@users.noreply.github.com>
2025-06-08 15:12:40 +0900

    [level 2] Title: 숫자의 표현, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -3,7 +3,7 @@ function solution(n) {
     for (let i = 1; i <= Math.sqrt(n); i++) {
         if (n % i === 0) {
             if (i % 2 !== 0) count++;
-            if (i !== n / i && (n / i) % 2 !== 0) count++;
+            if (i !== Math.sqrt(n) && (n / i) % 2 !== 0) count++;
         }
     }
     return count;
